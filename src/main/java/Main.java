4e54bc8534cfe903dbb550f6a498074efcf3217e
Friizy56 <dasangan882@gmail.com>
2026-06-18 23:36:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,20 +15,21 @@ public class Main {
             else if (input.startsWith("echo ")){
                 System.out.print(input.substring(5));
             }
+            
             else if(input.startsWith("type ")){
                 String command = input.substring(5);
                 if(command.matches("type|echo|exit")){
                     System.out.printf("%s is a shell builtin",command);
                 }
                 else{
-                    String path = System.getenv("Path");
+                    String path = System.getenv("PATH");
                     boolean found = false;
 
                     for(String dir : path.split(":")){
 
                         File file = new File(dir , command);
 
-                        if(file.exists() && file.canExecute()){
+                        if(file.isFile() && file.canExecute()){
                             System.out.printf("%s is %s", command, file.getAbsolutePath());
                             found = true;
                             break;
